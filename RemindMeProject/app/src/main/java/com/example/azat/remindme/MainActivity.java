1b0bd4e4d9291c5fabaeba0c18f30c1342af6873
Azat Galiullin <gali.azat@gmail.com>
2015-11-05 00:27:08 +0300

    mainActivity

---
+++ RemindMeProject/app/src/main/java/com/example/azat/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.azat.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Azat on 04.11.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
